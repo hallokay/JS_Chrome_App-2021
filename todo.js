a41e0ce5b1cc9e6f1277c2939281643332ab21ce
hallokay <hallokay044@gmail.com>
2021-08-11 22:50:19 +0900

    08.11.2021

--- todo.js
+++ todo.js
@@ -1,8 +1,67 @@
 const todoForm = document.getElementById("todo_form"),
+  todoInput = todoForm.querySelector("input"),
   todoList = document.getElementById("todo_list");
+
+ let todoArr = [];
+  const TODOS_KEY = "todos";
+
+  function saveTodo() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todoArr));
+  }
+
+
+  function deleteTodo(e) {
+    const li = e.target.parentElement;
+    li.remove();
+
+    // 로컬스토리지에서 지우기
+    
+
+  }
+
+
+function showTodo(todo) {
+  // console.log(todo);
+  const todoLi = document.createElement("li"),
+    todoTxt = document.createElement("span"),
+    todoBtn = document.createElement("button");
+
+    console.log(todo);
+    todoTxt.innerText = todo.text;
+    todoBtn.innerText = "❌";
+    todoLi.id = todo.id;
+    
+ todoLi.appendChild(todoTxt);
+ todoLi.appendChild(todoBtn);
+
+ todoList.appendChild(todoLi);
+
+
+todoBtn.addEventListener("click", deleteTodo);
+
+
+}
 
 function handleTodoSubmit(e) {
   e.preventDefault();
+  const newTodo = todoInput.value;
+  todoInput.value = "";
+
+  const newTodoObj = {
+    text: newTodo,
+    id:Date.now(),
+  }
+  todoArr.push(newTodoObj);
+  showTodo(newTodoObj);
+  saveTodo();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
+
+const savedTodo = localStorage.getItem(TODOS_KEY);
+
+if (savedTodo) {
+  const parsedTodo = JSON.parse(savedTodo);
+  todoArr = parsedTodo;
+  parsedTodo.forEach(showTodo);
+}
